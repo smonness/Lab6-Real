fc6e703ce64b323000e2c79163db8f21481db3a3
StrongKs <kentphipps99@gmail.com>
2023-03-10 10:11:18 -0500

    created decode_password() function and moved password_encoded assignment line into while loop to update value

--- software_engineering.py
+++ software_engineering.py
@@ -5,15 +5,18 @@ def encode_password(password):
         encoded_password += new_num
     return encoded_password
 
-
+# decode password function
 def decode_password(password):
-    pass
+    decoded_string = ""
+    for num in password:
+        decoded_num = str((int(num) - 3) % 10)
+        decoded_string += decoded_num
+    return decoded_string
 
 
 if __name__ == "__main__":
     Continue = True
     original_password = ""
-    password_encoded = encode_password(original_password)
     while Continue is True:
         print("\nMenu")
         print("-------------")
@@ -21,10 +24,11 @@ if __name__ == "__main__":
         print("2. Decode")
         print("3. Quit")
         option = int(input("\nPlease enter an option: "))
+        password_encoded = encode_password(original_password)  # moved into while loop to update value
         if option == 1:
             original_password = str(input("Please enter your password to encode: "))
             print("Your password has been encoded and stored!")
         elif option == 2:
-            print("The encoded password is ", encode_password(original_password), ", and the original password is ", decode_password(password_encoded), sep="")
+            print("The encoded password is ", encode_password(original_password), ", and the original password is ", decode_password(password_encoded), sep="", end=".\n")
         elif option == 3:
             break
